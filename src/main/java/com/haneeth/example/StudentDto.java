7f7ca6d43ebdc922456beaed32255f52eb98bec0
haneeth25 <captain857@gmail.com>
2024-10-27 20:57:54 +0530

    Beans methods

---
+++ src/main/java/com/haneeth/example/StudentDto.java
@@ -0,0 +1,48 @@
+package com.haneeth.example;
+
+public class StudentDto {
+
+    private String Firstname;
+    private String Lastname;
+    private int age;
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public StudentDto(String Firstname, String Lastname, int age, int id){
+        this.Firstname = Firstname;
+        this.Lastname = Lastname;
+        this.age = age;
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return Firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        Firstname = firstname;
+    }
+
+    public String getLastname() {
+        return Lastname;
+    }
+
+    public void setLastname(String lastname) {
+        Lastname = lastname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
